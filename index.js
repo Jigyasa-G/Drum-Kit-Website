c587e43bb72b1dd480fca5d5c5aebcc1631c14cd
copyNinja1219 <thapacyrus30@gmail.com>
2021-12-06 23:15:37 +0530

    added instructions

--- index.js
+++ index.js
@@ -1,76 +1,99 @@
 //Detecting Button Press
 
-var numberOfButtons = document.querySelectorAll(".drum").length ;
+var numberOfButtons = document.querySelectorAll('.drum').length;
 
-for(var i=0; i<numberOfButtons ; i++){
+for (var i = 0; i < numberOfButtons; i++) {
+  document.querySelectorAll('.drum')[i].addEventListener('click', handleClick);
 
-       document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
-
-       function handleClick(){
-
-         var buttonInnerHTML = this.innerHTML;
-         makeSound(buttonInnerHTML);
-         buttonAnimation(buttonInnerHTML);
-       }
-     }
+  function handleClick() {
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  }
+}
 
 //Detecting Keyboard Press
 
-document.addEventListener("keypress", function(event){
-makeSound(event.key);
-buttonAnimation(event.key);
+document.addEventListener('keypress', function (event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
 });
 
-function makeSound(key){
+function makeSound(key) {
   switch (key) {
-
-    case "w":                     //if w was the case
-    var audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
-    break;               //declarations between case and break:
+    case 'w': //if w was the case
+      var audio = new Audio('sounds/tom-1.mp3');
+      audio.play();
+      break; //declarations between case and break:
 
       break;
 
-    case "a":
-    var audio = new Audio("sounds/tom-2.mp3");
-    audio.play();
-    break;
+    case 'a':
+      var audio = new Audio('sounds/tom-2.mp3');
+      audio.play();
+      break;
 
-    case "s":
-    var audio = new Audio("sounds/tom-3.mp3");
-    audio.play();
-    break;
+    case 's':
+      var audio = new Audio('sounds/tom-3.mp3');
+      audio.play();
+      break;
 
-    case "d":
-    var audio = new Audio("sounds/tom-4.mp3");
-    audio.play();
-    break;
+    case 'd':
+      var audio = new Audio('sounds/tom-4.mp3');
+      audio.play();
+      break;
 
-    case "j":
-    var audio = new Audio("sounds/crash.mp3");
-    audio.play();
-    break;
+    case 'j':
+      var audio = new Audio('sounds/crash.mp3');
+      audio.play();
+      break;
 
-    case "k":
-    var audio = new Audio("sounds/kick-bass.mp3");
-    audio.play();
-    break;
+    case 'k':
+      var audio = new Audio('sounds/kick-bass.mp3');
+      audio.play();
+      break;
 
-    case "l":
-    var audio = new Audio("sounds/snare.mp3");
-    audio.play();
-    break;
+    case 'l':
+      var audio = new Audio('sounds/snare.mp3');
+      audio.play();
+      break;
 
-    default: console.log(buttonInnerHTML);
+    default:
+      console.log(buttonInnerHTML);
   }
-
 }
 
-function buttonAnimation(currentKey){
-var activeButton = document.querySelector("."+currentKey);
-activeButton.classList.add("pressed");
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector('.' + currentKey);
+  activeButton.classList.add('pressed');
 
-setTimeout(function(){
-  activeButton.classList.remove("pressed");
-}, 100 );
+  setTimeout(function () {
+    activeButton.classList.remove('pressed');
+  }, 100);
 }
+// modal js
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById('myBtn');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName('close')[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
