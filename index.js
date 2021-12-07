4fc7070a74e12c7813fec36f1ed908ffe6f3d955
copyNinja1219 <thapacyrus30@gmail.com>
2021-12-07 17:19:36 +0530

    added instructions and resolved conflicts

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-//Detecting Button Press
-
 var numberOfButtons = document.querySelectorAll('.drum').length;
 
 for (var i = 0; i < numberOfButtons; i++) {
@@ -26,20 +24,8 @@ function makeSound(key) {
       audio.play();
       break; //declarations between case and break:
 
-    case 'a':
-      var audio = new Audio('sounds/tom-2.mp3');
-      audio.play();
       break;
 
-    case 's':
-      var audio = new Audio('sounds/tom-3.mp3');
-      audio.play();
-      break;
-
-    case 'd':
-      var audio = new Audio('sounds/tom-4.mp3');
-      audio.play();
-      break;
     case 'a':
       var audio = new Audio('sounds/tom-2.mp3');
       audio.play();
@@ -80,6 +66,16 @@ function makeSound(key) {
       audio.play();
       break;
 
+    case 'f':
+      var audio = new Audio('sounds/openhat.wav');
+      audio.play();
+      break;
+
+    case 'h':
+      var audio = new Audio('sounds/boom.wav');
+      audio.play();
+      break;
+
     default:
       console.log(buttonInnerHTML);
   }
@@ -93,27 +89,21 @@ function buttonAnimation(currentKey) {
     activeButton.classList.remove('pressed');
   }, 100);
 }
-// modal js
-// Get the modal
+
 var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
 var btn = document.getElementById('myBtn');
 
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName('close')[0];
 
-// When the user clicks on the button, open the modal
 btn.onclick = function () {
   modal.style.display = 'block';
 };
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function () {
   modal.style.display = 'none';
 };
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = 'none';
