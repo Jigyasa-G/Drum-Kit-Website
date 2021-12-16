27b9c148bf0a0c1b2e6dbe05526fb5bb25930295
Abhinav Kumar Jha <1ds20cs003@dsce.edu.in>
2021-12-16 17:10:08 +0530

    Minor UX change

--- index.js
+++ index.js
@@ -51,25 +51,25 @@ document.addEventListener('keypress', function (event) {
 
 function makeSound(key) {
   switch (key) {
-    case 'w':
-      var audio = new Audio('sounds/tom-1.mp3');
-      audio.play();
-      break;
+    // case 'w':
+    //   var audio = new Audio('sounds/tom-1.mp3');
+    //   audio.play();
+    //   break;
 
-    case 'a':
-      var audio = new Audio('sounds/tom-2.mp3');
-      audio.play();
-      break;
+    // case 'a':
+    //   var audio = new Audio('sounds/tom-2.mp3');
+    //   audio.play();
+    //   break;
 
-    case 's':
-      var audio = new Audio('sounds/tom-3.mp3');
-      audio.play();
-      break;
+    // case 's':
+    //   var audio = new Audio('sounds/tom-3.mp3');
+    //   audio.play();
+    //   break;
 
-    case 'd':
-      var audio = new Audio('sounds/tom-4.mp3');
-      audio.play();
-      break;
+    // case 'd':
+    //   var audio = new Audio('sounds/tom-4.mp3');
+    //   audio.play();
+    //   break;
 
     case 'j':
       var audio = new Audio('sounds/crash.mp3');
@@ -81,10 +81,10 @@ function makeSound(key) {
       audio.play();
       break;
 
-    case 'l':
-      var audio = new Audio('sounds/snare.mp3');
-      audio.play();
-      break;
+    // case 'l':
+    //   var audio = new Audio('sounds/snare.mp3');
+    //   audio.play();
+    //   break;
 
     case 'i':
       var audio = new Audio('sounds/Hi-Hat-Foot-Pedal.mp3');
