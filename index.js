c6c0bb48fcfb4cc19670deb834a25dbfe1907589
Bushra Shireen <sbushra109@gmail.com>
2021-12-18 21:49:38 +0530

    Added the record sequence feature

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ function togglerefresh()
 
 //Detecting Keyboard Press
 
+let sequence=[];
 document.addEventListener('keypress', function (event) {
   makeSound(event.key);
   buttonAnimation(event.key);
@@ -125,9 +126,9 @@ function makeSound(key) {
       break;
 
     default:
-      console.log(buttonInnerHTML);
+      // console.log(buttonInnerHTML);
   }
-}
+  }
 
 function buttonAnimation(currentKey) {
   var activeButton = document.querySelector('.' + currentKey);
@@ -141,11 +142,16 @@ function buttonAnimation(currentKey) {
 var wrapper = document.getElementById('key_wrapper');
 
 var btn = document.getElementById('myBtn');
+var btn2= document.getElementById('myBtn2');
 
 
 
 btn.onclick = function () {
   wrapper.classList.toggle("key-map-wrapper_one");
+};
+
+btn2.onclick= function(){
+  document.getElementById('music-sequence').innerHTML = sequence;
 };
 
 
@@ -229,16 +235,42 @@ btn.onclick = function () {
       const keyNo = this.dataset.key;
       
       switch(keyNo) {
+        case '74':
+          sequence.push('J');
+          break;
+        case '66':
+          sequence.push('B');
+          break;
+        case '86':
+          sequence.push('V');
+          break;
+        case '72':
+          sequence.push('H');
+          break;
+        case '71':
+          sequence.push('G');
+          break;
+        case '70':
+          sequence.push('F');
+          break;
         case '69':
+          sequence.push('E');
+          break;
+        case '73':
+          sequence.push('I');
+          break;
         case '82':
           animateCrashOrRide();
+          sequence.push('R');
           break;
         case '75':
           animateHiHatClosed();
+          sequence.push('K');
           console.log("happen");
         break;
       }
       key.classList.add(playingClass);
+      console.log(sequence);
     }
   
     const drumbtn = document.querySelectorAll('.key');
